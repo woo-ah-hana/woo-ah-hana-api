28a7876be6d595155f9391e6e25eb70e3a332de5
함형주 <hhj01hsh9901@korea.ac.kr>
2025-01-05 20:11:48 +0900

    project setting

---
+++ woo-ah-hana-api/src/main/java/org/hana/wooahhanaapi/WooAhHanaApiApplication.java
@@ -0,0 +1,13 @@
+package org.hana.wooahhanaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooAhHanaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooAhHanaApiApplication.class, args);
+	}
+
+}
---
+++ woo-ah-hana-api/src/test/java/org/hana/wooahhanaapi/WooAhHanaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hana.wooahhanaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooAhHanaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
