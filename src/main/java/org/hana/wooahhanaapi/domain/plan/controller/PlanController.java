5417dded1feecda34bfd8ef767d02352a5e888f4
Choeseongeong <tjswjd7166@gmail.com>
2025-01-13 09:40:57 +0900

    [feat] plan 삭제하는 api 구현

--- src/main/java/org/hana/wooahhanaapi/domain/plan/controller/PlanController.java
+++ src/main/java/org/hana/wooahhanaapi/domain/plan/controller/PlanController.java
@@ -3,12 +3,7 @@ package org.hana.wooahhanaapi.domain.plan.controller;
 import lombok.RequiredArgsConstructor;
 import org.hana.wooahhanaapi.domain.plan.dto.CreatePlanRequestDto;
 import org.hana.wooahhanaapi.domain.plan.service.PlanService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -19,8 +14,14 @@ public class PlanController {
 
     private final PlanService planService;
 
-    @PostMapping
+    @PostMapping("/create")
     public UUID createPlan(@RequestBody CreatePlanRequestDto requestDto) {
         return planService.createPlan(requestDto);
+    }
+
+    @DeleteMapping("/{planId}")
+    public String deletePlan(@PathVariable String planId){
+        planService.deletePlan(planId);
+        return "Plan이 성공적으로 삭제되었습니다.";
     }
 }
