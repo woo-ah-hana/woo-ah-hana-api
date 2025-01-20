46e1283b33bb1448bbd8838975fb589b5d741df0
함형주 <hhj01hsh9901@korea.ac.kr>
2025-01-20 15:55:56 +0900

    [refactor] 테스트 코드 수정

--- src/test/java/org/hana/wooahhanaapi/adapter/AccountAdapterTest.java
+++ src/test/java/org/hana/wooahhanaapi/adapter/AccountAdapterTest.java
@@ -8,21 +8,27 @@ import org.hana.wooahhanaapi.domain.account.exception.TransferNotValidException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 
 @TestPropertySource(locations = "classpath:application-test.yml")
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 public class AccountAdapterTest {
     @Autowired
     private AccountAdapter accountAdapter;
 
     @BeforeEach
-    void seed() {
+    public void seed() {
         try{
             String bankTranId = "001"; //하나은행
             String accountType = "0";
@@ -38,18 +44,18 @@ public class AccountAdapterTest {
     @Test
     void createNewAccount() {
         // given
-        //이미 시딩된 account
-        String bankTranId = "003"; //하나은행
-        String accountType = "0";
-        String accountNumber = "2150094621845";
-        String productName = "자유입출금통장";
-        AccountCreateReqDto duplicatedAccount = new AccountCreateReqDto(bankTranId, accountType, accountNumber, productName);
-
         //새로운 account
         String newBankTranId ="111";
         Random rand = new Random();
         Long randomNumber = rand.nextLong(9999999999L);
         String newAccountNumber = "123" + String.valueOf(randomNumber);
+
+        //이미 시딩된 account
+        String bankTranId = "001"; //하나은행
+        String accountType = "0";
+        String accountNumber = "1111234123456";
+        String productName = "자유입출금통장";
+        AccountCreateReqDto duplicatedAccount = new AccountCreateReqDto(bankTranId, accountType, accountNumber, productName);
 
         AccountCreateReqDto newAccount = new AccountCreateReqDto(newBankTranId, accountType, newAccountNumber, productName);
 
@@ -84,7 +90,7 @@ public class AccountAdapterTest {
 
         //없는 계좌
         SimplifiedTransferReqDto notExistedReqDto = SimplifiedTransferReqDto.builder()
-                .accountNumber("111112312312")
+                .accountNumber("0000000000000")
                 .bankTranId("001")
                 .tranAmt("300000") //30만원 출금
                 .inoutType("입금")
@@ -106,17 +112,40 @@ public class AccountAdapterTest {
     @Test
     void getTransferRecord() {
         //given
+        SimplifiedTransferReqDto depositReqDto = SimplifiedTransferReqDto.builder()
+                .accountNumber("1111234123456")
+                .bankTranId("001")
+                .tranAmt("300000") //30만원 입금
+                .inoutType("입금")
+                .printContent("디지털 하나로 훈련 지원금")
+                .build();
+
+        SimplifiedTransferReqDto withdrawalReqDto = SimplifiedTransferReqDto.builder()
+                .accountNumber("1111234123456")
+                .bankTranId("001")
+                .tranAmt("300000") //30만원 출금
+                .inoutType("출금")
+                .printContent("밥플러스 1호점")
+                .build();
+        AccountTransferRespDto response1 = accountAdapter.createAccountTransfer(depositReqDto);
+        AccountTransferRespDto response2 = accountAdapter.createAccountTransfer(withdrawalReqDto);
+
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        // 날짜를 원하는 형식의 String으로 변환
+        String formattedDate = currentDate.format(formatter1);
+
         AccountTransferRecordReqDto reqDto = AccountTransferRecordReqDto.builder()
-                .bankTranId("003") // 하나은행
-                .accountNumber("2150094621845")
-                .fromDate("2024-01-02")
-                .toDate("2025-01-20")
+                .bankTranId("001") // 하나은행
+                .accountNumber("1111234123456")
+                .fromDate("2024-05-02")
+                .toDate(formattedDate)
                 .build();
 
         //존재하지 않는 계좌
         AccountTransferRecordReqDto notExistedReqDto = AccountTransferRecordReqDto.builder()
                 .bankTranId("001") // 하나은행
-                .accountNumber("8888888888877")
+                .accountNumber("0000000000000")
                 .fromDate("2024-01-02")
                 .toDate("2025-01-20")
                 .build();
@@ -134,10 +163,10 @@ public class AccountAdapterTest {
     @Test
     void getAccountInfo() {
         //given
-        GetAccountInfoReqDto reqDto = new GetAccountInfoReqDto("001","00","2025-01-17","2150094621845");
+        GetAccountInfoReqDto reqDto = new GetAccountInfoReqDto("001","00","2025-01-17","1111234123456");
 
         //존재하지 않는 계좌
-        GetAccountInfoReqDto notExistedReqDto = new GetAccountInfoReqDto("003","00","2025-01-17","8888888888877");
+        GetAccountInfoReqDto notExistedReqDto = new GetAccountInfoReqDto("003","00","2025-01-17","0000000000000");
 
         //when
         GetAccountInfoRespDto result = accountAdapter.getAccountInfo(reqDto);
